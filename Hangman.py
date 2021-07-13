b7e3215dad4734b2aa20477d162cfaf9e77d912a
Eli <87356905+eliatt9@users.noreply.github.com>
2021-07-14 01:44:28 +0300

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,32 +1,39 @@
 import random
 
+# General list of words to guess
 arr = ["eli", "zazu", "linoy", "davidtheking", "talma"]
-num = random.randint(0, len(arr)-1)
-word = arr[num]
 
-length = len(word)
-answer = []
-for le in range(length):
-    answer.append("_")
+# Choosing a random word
+word = random.choice(arr)
 
+# Initializing the array of the guesses to display to the user.
+answer = [" _ "] * len(word)
+
+# Variables for if the player guessed the word and how many lives the player has left.
 guess = False
 life = 8
 
+# Giving the player his guesses.
 while (not guess) and (life > 0):
     correct = True
     print("you still have %d lifespans" % life)
     print("".join(answer))
     user_input = input("My guess is:")
+    
+    # Checking if the user guessed correctly, and if so - replace the matching letter in the answer list.
     if user_input.isalpha() and len(user_input) == 1:
-        i = 0
-        while i < len(word):
+        correct = False
+        for i in range(len(word)):
             if user_input.lower() == word[i]:
                 answer[i] = user_input.lower()
-                correct = False
-            i += 1
-        if correct:
+                correct = True
+        
+        # If the user guessed wrong - reduce his life points
+        if not correct:
             life -= 1
-        if answer.count("_") == 0:
+           
+        # Checking if the player won - and if so, exit the loop.
+        if answer.count(" _ ") == 0:
             guess = True
     else:
         print("please enter a valid single letter")
